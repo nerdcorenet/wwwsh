830e0e1e217e23670a01f513922090e1e34e9f68
Mike Mallett <mike@nerdcore.net>
2016-11-26 13:33:26 -0500

    Added help text for server commands to cmd.php. Should consider removing from bin.js so that help on available server commands is stored only in the server-side PHP.

--- cmd.php
+++ cmd.php
@@ -1,7 +1,31 @@
 <?php
 header("Content-Type", "text/plain");
+// We expect to receive in the request string:
+//   cmd = The server command to be executed
+//   args = All following command line arguments to cmd
 switch($_REQUEST['cmd']) {
- case 'md5':
+  case 'help':
+    $helptxt = array(
+      'date' => "usage: server date<br><br>&nbsp;Prints the server date and time",
+      'md5' => "usage: server md5 [str]<br><br>&nbsp;Prints the MD5 of the specified string<br>&nbsp;Prompts for string using a password field if not specified",
+      'sha256' => "usage: server sha256 [str]<br><br>&nbsp;Prints the SHA256 of the specified string<br>&nbsp;Prompts for string using a password field if not specified",
+      'sha512' => "usage: server sha512 [str]<br><br>&nbsp;Prints the SHA512 of the specified string<br>&nbsp;Prompts for string using a password field if not specified",
+      'myinfo' => "usage: server myinfo<br><br>&nbsp;Prints information about your connection",
+      'test' => "usage: server test<br><br>&nbsp;Tests server communication",
+    );
+    if (isset($_REQUEST['args']) && $_REQUEST['args'] != 'null') {
+      $args = split(' ', $_REQUEST['args']);
+      if (strlen($args[0]) > 0) {
+        print $helptxt[$args[0]];
+      }
+    } else {
+      print 'This server supports the following commands:<br><br>';
+      foreach (array_keys($helptxt) as $hcmd) {
+        print '&nbsp;&nbsp;' . $hcmd . '<br>';
+      }
+    }
+    break;
+  case 'md5':
    //print md5(rawurldecode($_REQUEST['args']));
    print hash('md5', rawurldecode($_REQUEST['args']));
    break;
